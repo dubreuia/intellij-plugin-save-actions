b263020121fde6238e63631a3650cab0f365e9fd
Vojtech Krasa <vojta.krasa@gmail.com>
2015-05-06 16:03:23 +0200

    Deactivate plugin on document while typing
    Do not process the currently active editor unless the save was triggered
    manually

--- src/com/dubreuia/Component.java
+++ src/com/dubreuia/Component.java
@@ -5,19 +5,17 @@ import com.dubreuia.utils.PsiFiles;
 import com.intellij.AppTopics;
 import com.intellij.codeInsight.actions.AbstractLayoutCodeProcessor;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.components.*;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.fileEditor.FileDocumentManagerAdapter;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
-import com.intellij.util.messages.MessageBus;
-import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.annotations.NotNull;
-
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
+import com.intellij.openapi.project.*;
+import com.intellij.openapi.wm.IdeFrame;
+import com.intellij.openapi.wm.impl.IdeFrameImpl;
+import com.intellij.psi.*;
+import com.intellij.util.messages.*;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class Component implements ApplicationComponent {
 
@@ -33,8 +31,13 @@ public class Component implements ApplicationComponent {
 
     private FileDocumentManagerAdapter getFileDocumentManagerAdapter() {
         return new FileDocumentManagerAdapter() {
+
             @Override
             public void beforeDocumentSaving(@NotNull Document document) {
+                if (!isManuallyTriggered() && isActive(document)) {
+                    return;
+                }
+
                 for (Project project : ProjectManager.getInstance().getOpenProjects()) {
                     PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
                     if (isPsiFileEligible(project, psiFile)) {
@@ -47,6 +50,22 @@ public class Component implements ApplicationComponent {
                         }
                     }
                 }
+            }
+
+            private boolean isManuallyTriggered() {
+                return SaveAllAction.TRIGGERED;
+            }
+
+            private boolean isActive(@NotNull Document document) {
+                IdeFrame activeFrame = (IdeFrame) IdeFrameImpl.getActiveFrame();
+                return activeFrame != null && activeFrame.getProject() != null
+                  && isActive(activeFrame.getProject(), document);
+            }
+
+            private boolean isActive(@NotNull Project project, @NotNull Document document) {
+                Editor selectedTextEditor = FileEditorManagerEx.getInstance(project).getSelectedTextEditor();
+                //todo maybe check if it actually has focus
+                return selectedTextEditor != null && selectedTextEditor.getDocument() == document;
             }
         };
     }
