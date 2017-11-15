0c4c8744c6b522678f9362fb182f5a46ea94b875
markiewb <benno.markiewicz@googlemail.com>
2017-11-15 22:22:22 +0100

    Fixes: Using Win7 (at work) the button is displayed using a bright color while using the darcula theme.
    Use JButton implementation for the Reset button instead of the awt.Button.

--- src/com/dubreuia/ui/java/IdeSupportPanel.java
+++ src/com/dubreuia/ui/java/IdeSupportPanel.java
@@ -24,7 +24,7 @@ public class IdeSupportPanel {
 
     private static final String TITLE = "IDE support";
     private static final String LABEL = "Use external Eclipse configuration file (.epf)";
-    private static final String BUTTON = "reset";
+    private static final String BUTTON = "Reset";
 
     private TextFieldWithBrowseButton path;
 
@@ -39,7 +39,7 @@ public class IdeSupportPanel {
 
         JBLabel label = getLabel();
         path = getPath(configurationPath);
-        Button reset = getResetButton(path);
+        JButton reset = getResetButton(path);
 
         panel.add(label, BorderLayout.WEST);
         panel.add(path, BorderLayout.CENTER);
@@ -70,8 +70,8 @@ public class IdeSupportPanel {
     }
 
     @NotNull
-    private Button getResetButton(final TextFieldWithBrowseButton path) {
-        Button reset = new Button(BUTTON);
+    private JButton getResetButton(final TextFieldWithBrowseButton path) {
+        JButton reset = new JButton(BUTTON);
         reset.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
