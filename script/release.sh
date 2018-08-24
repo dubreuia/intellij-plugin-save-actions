89d9bdb4e8d0317dea88e5822974818bfeb48aa9
Alexandre Dubreuil <dubreuil.alex@gmail.com>
2018-08-24 14:30:50 +0200

    Add release script to change versions

---
+++ script/release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VERSION_NEXT=${1?"Usage: $0 VERSION_NEXT"}
+VERSION_CURRENT="$( grep "^version" build.gradle | grep -oP "version \K[\w\.']+" | sed "s/'//g" )"
+
+for file in build.gradle docs/badge-jetbrains-website.svg
+do
+    sed "s/${VERSION_CURRENT}/${VERSION_NEXT}/g" ${file} > ${file}.next && mv ${file}.next ${file}
+done
