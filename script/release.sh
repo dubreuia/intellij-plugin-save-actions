66f2da768d4d053747a814f78a5ab90469375f92
Alexandre DuBreuil <dubreuil.alex@gmail.com>
2018-08-31 17:41:06 +0200

    Promote to version 1.1.0

--- script/release.sh
+++ script/release.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 VERSION_NEXT=${1?"Usage: $0 VERSION_NEXT"}
-VERSION_CURRENT="$( grep "^version" build.gradle | grep -oP "version \K[\w\.']+" | sed "s/'//g" )"
+VERSION_CURRENT="$( grep "^version" build.gradle |
+                    grep -oP "version \K[\w\.']+" |
+                    sed "s/'//g" )"
 
-for file in build.gradle docs/badge-jetbrains-website.svg
+for file in "build.gradle" \
+            "docs/badge-jetbrains-website.svg" \
+            "src/main/resources/META-INF/plugin.xml"
 do
     sed "s/${VERSION_CURRENT}/${VERSION_NEXT}/g" ${file} > ${file}.next && mv ${file}.next ${file}
 done
+
