f141f7f1869621ce5f11193797677c8f836bf629
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-09-11 17:54:29 -0400

    Fix #194: Add copyright to java files

--- src/main/java/com/dubreuia/ui/GeneralPanel.java
+++ src/main/java/com/dubreuia/ui/GeneralPanel.java
@@ -1,10 +1,38 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Alexandre DuBreuil
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package com.dubreuia.ui;
 
 import com.dubreuia.model.Action;
 import com.intellij.ui.IdeBorderFactory;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import java.awt.Dimension;
 import java.util.Map;
 
 import static com.dubreuia.model.Action.activate;
