d2cfff16f923787feb5d859b6a03557cdced0993
Fishermans <zu.mir@xyz.de>
2023-01-26 20:26:31 +0100

    fixed #419 on 2022.3

--- src/main/java/com/siyeh/ig/style/CustomUnqualifiedStaticUsageInspection.java
+++ src/main/java/com/siyeh/ig/style/CustomUnqualifiedStaticUsageInspection.java
@@ -25,8 +25,6 @@
 
 package com.siyeh.ig.style;
 
-import org.jetbrains.annotations.NotNull;
-
 import com.intellij.psi.JavaResolveResult;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
@@ -43,6 +41,8 @@ import com.intellij.psi.PsiSwitchLabelStatement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
 import com.siyeh.ig.BaseInspectionVisitor;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Copy pasting because: cannot extend. Do not reformat (useful for diffs)
@@ -51,89 +51,104 @@ import com.siyeh.ig.BaseInspectionVisitor;
  */
 public class CustomUnqualifiedStaticUsageInspection extends UnqualifiedStaticUsageInspection {
 
+  @Override
+  public @Nls(capitalization = Nls.Capitalization.Sentence) @NotNull String getDisplayName() {
+
+    return this.getClass().getSimpleName();
+  }
+
+
+  @Override
+  public BaseInspectionVisitor buildVisitor() {
+
+    return new CustomUnqualifiedStaticCallVisitor();
+  }
+
+
+  private class CustomUnqualifiedStaticCallVisitor extends BaseInspectionVisitor {
+
     @Override
-    public BaseInspectionVisitor buildVisitor() {
-        return new CustomUnqualifiedStaticCallVisitor();
-    }
+    public void visitMethodCallExpression(
+        @NotNull final PsiMethodCallExpression expression) {
 
-    private class CustomUnqualifiedStaticCallVisitor extends BaseInspectionVisitor {
-      @Override
-      public void visitMethodCallExpression(
-        @NotNull PsiMethodCallExpression expression) {
-        super.visitMethodCallExpression(expression);
-        if (m_ignoreStaticMethodCalls) {
-          return;
-        }
-        final PsiReferenceExpression methodExpression =
+      super.visitMethodCallExpression(expression);
+      if (m_ignoreStaticMethodCalls) {
+        return;
+      }
+      final PsiReferenceExpression methodExpression =
           expression.getMethodExpression();
-        if (!isUnqualifiedStaticAccess(methodExpression)) {
-          return;
-        }
-        registerError(methodExpression, expression);
+      if (!isUnqualifiedStaticAccess(methodExpression)) {
+        return;
       }
-
-      @Override
-      public void visitReferenceExpression(
-        @NotNull PsiReferenceExpression expression) {
-        super.visitReferenceExpression(expression);
-        if (m_ignoreStaticFieldAccesses) {
-          return;
-        }
-        final PsiElement element = expression.resolve();
-        if (!(element instanceof PsiField)) {
-          return;
-        }
-        final PsiField field = (PsiField)element;
-        if (field.hasModifierProperty(PsiModifier.FINAL) &&
-            PsiUtil.isOnAssignmentLeftHand(expression)) {
-          return;
-        }
-        if (!isUnqualifiedStaticAccess(expression)) {
-          return;
-        }
-        registerError(expression, expression);
-      }
-
-      private boolean isUnqualifiedStaticAccess(
-        PsiReferenceExpression expression) {
-        if (m_ignoreStaticAccessFromStaticContext) {
-          final PsiMember member =
-            PsiTreeUtil.getParentOfType(expression,
-                                        PsiMember.class);
-          if (member != null &&
-              member.hasModifierProperty(PsiModifier.STATIC)) {
-            return false;
-          }
-        }
-        final PsiExpression qualifierExpression =
-          expression.getQualifierExpression();
-        if (qualifierExpression != null) {
-          return false;
-        }
-        final JavaResolveResult resolveResult =
-          expression.advancedResolve(false);
-        final PsiElement currentFileResolveScope =
-          resolveResult.getCurrentFileResolveScope();
-        if (currentFileResolveScope instanceof PsiImportStaticStatement) {
-          return false;
-        }
-        final PsiElement element = resolveResult.getElement();
-        if (!(element instanceof PsiField) &&
-            !(element instanceof PsiMethod)) {
-          return false;
-        }
-        final PsiMember member = (PsiMember)element;
-        if (member instanceof PsiEnumConstant &&
-            expression.getParent() instanceof PsiSwitchLabelStatement) {
-          return false;
-        }
-        PsiClass expressionClass = PsiTreeUtil.getParentOfType(expression, PsiClass.class);
-        PsiClass memberClass = member.getContainingClass();
-        if (memberClass != null && memberClass.equals(expressionClass)) {
-          return false;
-        }
-        return member.hasModifierProperty(PsiModifier.STATIC);
-      }
+      registerError(methodExpression, expression);
     }
+
+
+    @Override
+    public void visitReferenceExpression(
+        @NotNull final PsiReferenceExpression expression) {
+
+      super.visitReferenceExpression(expression);
+      if (m_ignoreStaticFieldAccesses) {
+        return;
+      }
+      final PsiElement element = expression.resolve();
+      if (!(element instanceof PsiField)) {
+        return;
+      }
+      final PsiField field = (PsiField) element;
+      if (field.hasModifierProperty(PsiModifier.FINAL) &&
+          PsiUtil.isOnAssignmentLeftHand(expression)) {
+        return;
+      }
+      if (!isUnqualifiedStaticAccess(expression)) {
+        return;
+      }
+      registerError(expression, expression);
+    }
+
+
+    private boolean isUnqualifiedStaticAccess(
+        final PsiReferenceExpression expression) {
+
+      if (m_ignoreStaticAccessFromStaticContext) {
+        final PsiMember member =
+            PsiTreeUtil.getParentOfType(expression,
+                PsiMember.class);
+        if (member != null &&
+            member.hasModifierProperty(PsiModifier.STATIC)) {
+          return false;
+        }
+      }
+      final PsiExpression qualifierExpression =
+          expression.getQualifierExpression();
+      if (qualifierExpression != null) {
+        return false;
+      }
+      final JavaResolveResult resolveResult =
+          expression.advancedResolve(false);
+      final PsiElement currentFileResolveScope =
+          resolveResult.getCurrentFileResolveScope();
+      if (currentFileResolveScope instanceof PsiImportStaticStatement) {
+        return false;
+      }
+      final PsiElement element = resolveResult.getElement();
+      if (!(element instanceof PsiField) &&
+          !(element instanceof PsiMethod)) {
+        return false;
+      }
+      final PsiMember member = (PsiMember) element;
+      if (member instanceof PsiEnumConstant &&
+          expression.getParent() instanceof PsiSwitchLabelStatement) {
+        return false;
+      }
+      final PsiClass expressionClass = PsiTreeUtil.getParentOfType(expression, PsiClass.class);
+      final PsiClass memberClass = member.getContainingClass();
+      if (memberClass != null && memberClass.equals(expressionClass)) {
+        return false;
+      }
+      return member.hasModifierProperty(PsiModifier.STATIC);
+    }
+  }
 
 }
